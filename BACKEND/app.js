22a06020b9245c81ee468666641b40a5eb6992db
Aditya Vidhuri <vidhuriaditya10@gmail.com>
2026-05-21 12:08:06 +0530

    Implemented complete backend setup for URL shortener

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,17 +1,35 @@
 import express from 'express';
-import { nanoid } from 'nanoid';
+import dotenv from 'dotenv';
+
+import connectDB from './src/config/mongo.config.js';
+import shortUrl from './src/routes/shortUrl.route.js';
+import { redirectFromShortUrl } from './src/controller/shortUrl.controller.js';
+import errorMiddleware from './src/Middlewares/error.middleware.js';
+
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/create', (req, res) => {
-    res.send(nanoid(7));
-});
-app.post('/api/create', (req, res) => {
-    const {url} = req.body;
-    res.send(nanoid(7));
-});
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.use('/api/create', shortUrl);
+
+app.get('/:shortUrl', redirectFromShortUrl);
+
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+    .then(() => {
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed:", error);
+    });
+    
